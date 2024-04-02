b777d15d2833abbbb77eacbae20c512125bd8894
김민재 <kimminje7267@naver.com>
2024-04-02 18:08:35 +0900

    chore: image/next 설정, cors 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "crests.football-data.org",
+        port: "",
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `https://api.football-data.org/v4/:path*`,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
