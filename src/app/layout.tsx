fd5eed8ed75f7761be4106265342b204f4be8dd4
김민재 <kimminje7267@naver.com>
2024-04-03 16:29:21 +0900

    design: layout style 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className="flex min-h-screen flex-col items-center gap-5">
+          <div className="flex justify-start">{children}</div>
+        </main>
+      </body>
     </html>
   );
 }
