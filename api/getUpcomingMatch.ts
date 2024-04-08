1fe67c34fcd253793d2f48a36c010639f853ec9a
김민재 <kimminje7267@naver.com>
2024-04-08 16:37:56 +0900

    refactor: getUpComingMatch 함수 return 값 변경

--- api/getUpcomingMatch.ts
+++ api/getUpcomingMatch.ts
@@ -1,5 +1,3 @@
-import { Match } from "@/types/Match";
-
 const getUpcomingMatch = async () => {
   const url = `api/matches`;
 
@@ -17,15 +15,7 @@ const getUpcomingMatch = async () => {
 
   const data = await res.json();
 
-  const upComingMatch = data.matches.filter(
-    (match: Match) =>
-      match.competition.code === "PL" || // 프리미어 리그
-      match.competition.code === "CL" || // 챔피언스 리그
-      match.competition.code === "FL1" || // 프랑스 리그1
-      match.competition.code === "BL1" // 분데스리가
-  );
-
-  return upComingMatch;
+  return data.matches;
 };
 
 export default getUpcomingMatch;
