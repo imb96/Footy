70fa7071aaa66c65c65d874d6e481fcab09855bc
김민재 <kimminje7267@naver.com>
2024-04-08 16:38:24 +0900

    refactor: main page 스타일, 렌더링 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-"use client";
-
 import UpComingMatches from "@/components/UpComingMatches";
+import PlayerRank from "@/components/PlayerRank";
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex min-h-screen flex-col gap-5 pt-5">
       <UpComingMatches />
+      <PlayerRank />
     </main>
   );
 }
