0c81e9a17e3b2ca2345a527c2fb3b2669775d191
김민재 <kimminje7267@naver.com>
2024-04-09 16:22:21 +0900

    feat: UpComingMatch pl, cl 구분해서 출력, league image 추가

--- src/components/UpComingMatches.tsx
+++ src/components/UpComingMatches.tsx
@@ -1,19 +1,23 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import getUpcomingMatch from "@/api/getUpcomingMatch";
+import getSchedule from "@/api/getSchedule";
 import MatchCard from "./MatchCard";
 
 import { Match } from "@/types/Match";
+import Image from "next/image";
 
 const UpComingMatches = () => {
-  const [data, setData] = useState<Match[]>([]);
+  const [plMatches, setPLMatches] = useState<Match[]>([]);
+  const [clMatches, setCLMatches] = useState<Match[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const upcomingMatchData = await getUpcomingMatch();
-        setData(upcomingMatchData);
+        const upComingCLMatches = await getSchedule({ competitions: "CL" });
+        const upComingPLMatches = await getSchedule({ competitions: "PL" });
+        setCLMatches(upComingCLMatches);
+        setPLMatches(upComingPLMatches);
       } catch (error) {
         console.error("Error fetching upcoming match data:", error);
       }
@@ -24,10 +28,18 @@ const UpComingMatches = () => {
 
   return (
     <div>
-      {"Upcoming Matches"}
-      {data.length > 0 ? (
+      {plMatches.length > 0 && (
         <div>
-          {data.map((match) => (
+          <div className="flex gap-1 items-end text-xl">
+            <Image
+              src={plMatches[0].competition.emblem}
+              alt={"league emblem"}
+              width={24}
+              height={24}
+            />
+            {"Premier League"}
+          </div>
+          {plMatches.map((match) => (
             <MatchCard
               key={match.id}
               date={match.utcDate}
@@ -45,8 +57,36 @@ const UpComingMatches = () => {
             />
           ))}
         </div>
-      ) : (
-        <div>Loading...</div>
+      )}
+      {clMatches.length > 0 && (
+        <div>
+          <div className="flex gap-1 items-end text-xl">
+            <Image
+              src={clMatches[0].competition.emblem}
+              alt={"league emblem"}
+              width={24}
+              height={24}
+            />
+            {"Champions League"}
+          </div>
+          {clMatches.map((match) => (
+            <MatchCard
+              key={match.id}
+              date={match.utcDate}
+              status={match.status}
+              homeTeamName={match.homeTeam.name}
+              homeTeamShortName={match.homeTeam.shortName}
+              homeTeamTla={match.homeTeam.tla}
+              homeTeamCrest={match.homeTeam.crest}
+              homeTeamScore={match.score.fullTime.home}
+              awayTeamName={match.awayTeam.name}
+              awayTeamShortName={match.awayTeam.shortName}
+              awayTeamTla={match.awayTeam.tla}
+              awayTeamCrest={match.awayTeam.crest}
+              awayTeamScore={match.score.fullTime.away}
+            />
+          ))}
+        </div>
       )}
     </div>
   );
