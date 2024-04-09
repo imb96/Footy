9e74f640bb902a457b94705c380c7b72bb63ae7e
김민재 <kimminje7267@naver.com>
2024-04-09 16:21:30 +0900

    feat: getSchedule api 함수, 필터링 함수 작성

---
+++ api/getSchedule.ts
@@ -0,0 +1,46 @@
+import { Match } from "@/types/Match";
+
+const getSchedule = async ({ competitions }: { competitions: string }) => {
+  const url = `api/competitions/${competitions}/matches?status=SCHEDULED`;
+
+  const res = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Auth-Token": process.env.NEXT_PUBLIC_API_KEY || "",
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error("[getSchedule] api Failed to fetch data");
+  }
+
+  const data = await res.json();
+  const currentDate = new Date();
+  const today = new Date(currentDate);
+  const yesterday = new Date(currentDate);
+  const tomorrow = new Date(currentDate);
+  yesterday.setDate(currentDate.getDate() - 1);
+  tomorrow.setDate(currentDate.getDate() + 1);
+
+  const isSameDate = (date1: Date, date2: Date) => {
+    return (
+      date1.getFullYear() === date2.getFullYear() &&
+      date1.getMonth() === date2.getMonth() &&
+      date1.getDate() === date2.getDate()
+    );
+  };
+
+  const filteredMatches = data.matches.filter((match: Match) => {
+    const matchDate = new Date(match.utcDate);
+    return (
+      isSameDate(matchDate, yesterday) ||
+      isSameDate(matchDate, today) ||
+      isSameDate(matchDate, tomorrow)
+    );
+  });
+
+  return filteredMatches;
+};
+
+export default getSchedule;
