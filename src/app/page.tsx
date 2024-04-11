129923a85961e428afbcd006efc5ff69fced1530
김민재 <kimminje7267@naver.com>
2024-04-11 16:29:27 +0900

    feat: QueryClientProvider 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
+"use client";
 import UpComingMatches from "@/components/UpComingMatches";
-import PlayerRank from "@/components/PlayerRank";
+import { QueryClientProvider } from "@tanstack/react-query";
+import queryClient from "@/hooks/api/queryClient";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col gap-5">
-      <UpComingMatches />
-      <PlayerRank />
+      <QueryClientProvider client={queryClient}>
+        <UpComingMatches />
+      </QueryClientProvider>
     </main>
   );
 }
