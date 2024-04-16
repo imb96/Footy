1e26ef4996bfa2bd556c5d33717ded19fe0070f3
김민재 <kimminje7267@naver.com>
2024-04-16 16:53:29 +0900

    feat: Header에 HeaderItem 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,14 @@
+import HeaderItem from './HeaderItem'
 import Logo from './Logo'
 
 const Header = () => {
   return (
     <div className="top-0 sticky py-5 px-14 w-full flex items-center gap-12 bg-rose-500 opacity-80">
       <Logo />
+      <div className="flex gap-10">
+        <HeaderItem imageSrc="/images/pl.png" link="/pl" />
+        <HeaderItem imageSrc="/images/cl.png" link="/cl" />
+      </div>
     </div>
   )
 }
