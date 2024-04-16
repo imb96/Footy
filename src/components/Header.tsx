4e0403ca5b9ad6182b76cefcaf313cf7a99df8ac
김민재 <kimminje7267@naver.com>
2024-04-16 15:57:37 +0900

    feat: Header 컴포넌트 작성

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import Logo from './Logo'
+
+const Header = () => {
+  return (
+    <div className="top-0 sticky py-5 px-14 w-full flex items-center gap-12 bg-rose-500 opacity-80">
+      <Logo />
+    </div>
+  )
+}
+
+export default Header
