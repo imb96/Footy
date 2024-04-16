c6a655b5f57bc71dd28424c1bf0e7946eaa514cd
김민재 <kimminje7267@naver.com>
2024-04-16 16:54:16 +0900

    refactor: formatDate 날짜 포맷팅 변경

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,12 +1,14 @@
 const formatDate = (datetimeString: string) => {
+  const weekdays = ['일', '월', '화', '수', '목', '금', '토']
   const date = new Date(datetimeString)
   const year = date.getFullYear().toString().slice(-2)
   const month = (date.getMonth() + 1).toString().padStart(2, '0')
   const day = date.getDate().toString().padStart(2, '0')
   const hours = date.getHours().toString().padStart(2, '0')
   const minutes = date.getMinutes().toString().padStart(2, '0')
+  const weekday = weekdays[date.getDay()] // 요일 구하기
 
-  return `${year}/${month}/${day} ${hours}:${minutes}`
+  return `${month}.${day} ${weekday} ${hours}:${minutes}`
 }
 
 export default formatDate
