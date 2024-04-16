9b3ea7b13f94725ea283ff6456bf8fda68800dc8
김민재 <kimminje7267@naver.com>
2024-04-16 15:57:15 +0900

    feat: Logo 컴포넌트 작성

---
+++ src/components/Logo.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+
+const Logo = () => {
+  const router = useRouter()
+  const handleClickLogo = () => {
+    router.push('/')
+  }
+  return (
+    <div
+      className="text-2xl text-white font-black cursor-pointer"
+      onClick={handleClickLogo}
+    >
+      Footy
+    </div>
+  )
+}
+
+export default Logo
