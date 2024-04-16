363686c013d62c467d69437bab38e80d09f355f4
김민재 <kimminje7267@naver.com>
2024-04-16 15:57:50 +0900

    feat: layout에 Header 컴포넌트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
+import Header from '@/components/Header'
+
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -18,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Header />
         <main className="flex min-h-screen flex-col items-center gap-5 p-5">
           <div>{children}</div>
         </main>
