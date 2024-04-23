ef235ba5a4dca12d551a283380a340153d316df5
김민재 <kimminje7267@naver.com>
2024-04-23 17:20:33 +0900

    feat: PL 페이지에 PlayerRank 컴포넌트 사용

--- src/app/pl/page.tsx
+++ src/app/pl/page.tsx
@@ -1,9 +1,11 @@
+import PlayerRank from '@/components/PlayerRank'
 import TeamRank from '@/components/TeamRank'
 
 export default function PlPage() {
   return (
     <main className="flex min-h-screen flex-col gap-5">
       <TeamRank competition="PL" />
+      <PlayerRank />
     </main>
   )
 }
