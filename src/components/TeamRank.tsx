197e1309025f9ea0e0bda0c8c3bf9f7a78b92c50
김민재 <kimminje7267@naver.com>
2024-04-23 15:27:34 +0900

    feat: TeamRank 항목 라벨링

--- src/components/TeamRank.tsx
+++ src/components/TeamRank.tsx
@@ -24,30 +24,55 @@ const TeamRank = ({ competition }: { competition: string }) => {
 
   return (
     <div className="flex flex-col gap-2">
-      <div className="text-lg">{'Premier League Team Rank'}</div>
+      <div className="flex gap-1 items-center">
+        <div className="flex justify-center gap-1 items-center p-5 truncate">
+          <Image
+            src={'/images/PL.png'}
+            alt={'league emblem'}
+            width={40}
+            height={40}
+          />
+          <h1 className="text-2xl font-bold">{'Premier League Team Rank'}</h1>
+        </div>
+      </div>
+      <div className="flex gap-2 items-center text-center font-bold">
+        <div className="text-sm w-4"></div>
+        <div className="text-sm md:w-44 sm:w-40 w-28">클럽</div>
+        <div className="text-sm w-5">경기</div>
+        <div className="text-sm w-5">승</div>
+        <div className="text-sm w-5">무</div>
+        <div className="text-sm w-5">패</div>
+        <div className="text-sm w-5">득점</div>
+        <div className="text-sm w-5">실점</div>
+        <div className="text-sm w-5">득실</div>
+        <div className="text-sm w-5">승점</div>
+      </div>
       {teams.map((team: Team) => (
-        <div key={team.position} className="flex gap-2 items-center">
-          <div>{team.position}</div>
+        <div
+          key={team.position}
+          className="flex gap-2 items-center text-center"
+        >
+          <div className="text-sm w-4">{team.position}</div>
           <Image
             src={team.team.crest}
             alt={team.team.name}
             width={24}
             height={24}
           />
-          <div className="truncate text-xs hidden md:inline">
+          <div className="truncate text-xs hidden md:inline w-36 text-start">
             {team.team.name}
           </div>
-          <div className="truncate hidden sm:inline md:hidden text-xs">
+          <div className="truncate hidden sm:inline md:hidden text-xs w-32">
             {team.team.shortName}
           </div>
-          <div className="text-xs sm:hidden">{team.team.tla}</div>
-          <div className="text-sm">{team.playedGames}</div>
-          <div className="text-sm">{team.won}</div>
-          <div className="text-sm">{team.draw}</div>
-          <div className="text-sm">{team.lost}</div>
-          <div className="text-sm">{team.goalsFor}</div>
-          <div className="text-sm">{team.goalsAgainst}</div>
-          <div className="text-sm">{team.goalDifference}</div>
+          <div className="text-xs sm:hidden w-20">{team.team.tla}</div>
+          <div className="text-sm w-5">{team.playedGames}</div>
+          <div className="text-sm w-5">{team.won}</div>
+          <div className="text-sm w-5">{team.draw}</div>
+          <div className="text-sm w-5">{team.lost}</div>
+          <div className="text-sm w-5">{team.goalsFor}</div>
+          <div className="text-sm w-5">{team.goalsAgainst}</div>
+          <div className="text-sm w-5">{team.goalDifference}</div>
           <div className="text-sm font-bold">{team.points}</div>
         </div>
       ))}
