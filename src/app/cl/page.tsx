62aaf32e5199f1237632cc42dce85f240f89ed5e
김민재 <kimminje7267@naver.com>
2024-05-01 15:37:15 +0900

    feat: CL page에 CL PlayerRank 출력

--- src/app/cl/page.tsx
+++ src/app/cl/page.tsx
@@ -1,3 +1,9 @@
+import PlayerRank from '@/components/PlayerRank'
+
 export default function ClPage() {
-  return <main className="flex min-h-screen flex-col gap-5">{'In Ready'}</main>
+  return (
+    <main className="flex min-h-screen flex-col gap-5">
+      <PlayerRank competition="CL" />
+    </main>
+  )
 }
