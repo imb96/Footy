f43e616783be24eba1ffbd9cc33d1b3ce864a831
김민재 <kimminje7267@naver.com>
2024-05-23 15:08:10 +0900

    feat: upcoming match 없을 때 페이지 이동 추가

--- src/components/UpComingMatches.tsx
+++ src/components/UpComingMatches.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 
 import useScheduleQuery from '@/hooks/api/useScheduleQuery'
 import { Match } from '@/types/Match'
@@ -8,6 +9,8 @@ import { Match } from '@/types/Match'
 import MatchCard from './MatchCard'
 
 const UpComingMatches = ({ competition }: { competition: string }) => {
+  const router = useRouter()
+
   const {
     data: matches,
     isLoading,
@@ -28,7 +31,7 @@ const UpComingMatches = ({ competition }: { competition: string }) => {
 
   return (
     <div className="flex flex-col gap-10">
-      {matches.length > 0 && (
+      {matches.length > 0 ? (
         <div>
           <div className="flex justify-center gap-2 items-center p-5 truncate">
             <Image
@@ -58,6 +61,14 @@ const UpComingMatches = ({ competition }: { competition: string }) => {
               awayTeamScore={match.score.fullTime.away}
             />
           ))}
+        </div>
+      ) : (
+        <div>
+          <div>{`No upcoming ${competition} matches...`}</div>
+          <div
+            className="cursor-pointer"
+            onClick={() => router.push(`/${competition.toLowerCase()}`)}
+          >{` -> Go to ${competition} Ranking`}</div>
         </div>
       )}
     </div>
