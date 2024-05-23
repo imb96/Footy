f2cfcd0eea2d9aa311295e755153a795a3a18280
김민재 <kimminje7267@naver.com>
2024-05-23 15:33:10 +0900

    feat: son image 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import Image from 'next/image'
+
 import UpComingMatches from '@/components/UpComingMatches'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col gap-5">
+      <Image src="/images/son.png" alt="son" width={200} height={200} />
       <UpComingMatches competition={'PL'} />
       <UpComingMatches competition={'CL'} />
     </main>
