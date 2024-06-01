5d30708494bc257075f15d7015a72b33973af9db
김민재 <kimminje7267@naver.com>
2024-06-01 16:03:36 +0900

    feat: Button 컴포넌트 작성

---
+++ src/components/Button.tsx
@@ -0,0 +1,21 @@
+import type { ComponentPropsWithRef, ForwardedRef } from 'react'
+import { forwardRef } from 'react'
+
+export interface ButtonProps extends ComponentPropsWithRef<'button'> {
+}
+
+const Button = (
+  { children, ...attributes }: ButtonProps,
+  ref: ForwardedRef<HTMLButtonElement>,
+) => {
+  return (
+    <button
+      ref={ref}
+      {...attributes}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default forwardRef(Button)
