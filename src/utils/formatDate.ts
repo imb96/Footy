61e7529d93b95b22264c1d29798158087ac74c67
김민재 <kimminje7267@naver.com>
2024-06-02 14:59:12 +0900

    style: 불필요한 코드 삭제

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,7 +1,5 @@
 const formatDate = (date: Date) => {
   const WEEK_DAYS = ['일', '월', '화', '수', '목', '금', '토'] as const
-  // const date = new Date(datetimeString)
-  const year = date.getFullYear().toString().slice(-2)
   const month = (date.getMonth() + 1).toString().padStart(2, '0')
   const day = date.getDate().toString().padStart(2, '0')
   const hours = date.getHours().toString().padStart(2, '0')
